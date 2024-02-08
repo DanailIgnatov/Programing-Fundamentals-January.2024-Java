05f90c0d857256affc743add619a20db6922d67a
Danail Ignatov <151131108+DanailIgnatov@users.noreply.github.com>
2024-02-08 16:26:42 +0200

    Add files via upload

--- Lists_Lab/P04ListManipulationAdvanced.java
+++ Lists_Lab/P04ListManipulationAdvanced.java
@@ -1,4 +1,10 @@
 package Lists_Lab;
 
+import java.util.Scanner;
+
 public class P04ListManipulationAdvanced {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+    }
 }
