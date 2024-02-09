2a7bd98d328c5fa7b7c6d1688f2327252f726a3d
Danail Ignatov <151131108+DanailIgnatov@users.noreply.github.com>
2024-02-09 15:56:10 +0200

    Add files via upload

--- Lists_Lab/P06RemoveNegativesAndReverse.java
+++ Lists_Lab/P06RemoveNegativesAndReverse.java
@@ -1,4 +1,26 @@
 package Lists_Lab;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class P06RemoveNegativesAndReverse {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<Integer> numbers = Arrays.stream(scanner.nextLine().split(" "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+        List<Integer> positiveNumbers = new ArrayList<>();
+        for (int number : numbers) {
+            if (number > 0) {
+                positiveNumbers.add(number);
+            }
+        }
+        if (positiveNumbers.isEmpty()){
+            System.out.println("empty");
+            return;
+        }
+        Collections.reverse(positiveNumbers);
+        System.out.println(positiveNumbers.toString().replaceAll("[\\[\\],]", ""));
+    }
 }
