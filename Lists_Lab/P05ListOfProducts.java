017bc0b4818cf4469cbce2c7cc8abd23a79d4904
Danail Ignatov <151131108+DanailIgnatov@users.noreply.github.com>
2024-02-11 10:52:34 +0200

    Add files via upload

--- Lists_Lab/P05ListOfProducts.java
+++ Lists_Lab/P05ListOfProducts.java
@@ -1,4 +1,23 @@
 package Lists_Lab;
 
+import java.util.*;
+
 public class P05ListOfProducts {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+        List<String> products = new ArrayList<>();
+
+        for (int i = 1; i <= n; i++) {
+            String product = scanner.nextLine();
+            products.add(product);
+        }
+        Collections.sort(products);
+        int frontNumber = 1;
+        for (String number : products) {
+            System.out.println(frontNumber + "." + number);
+            frontNumber++;
+        }
+    }
 }
